3b4eb312398ae0f214fbd063b281b07b2ec1ddd7
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:43:41 -0400

    Added Private Instance Variables

--- Abstractplanets/TerrestrialPlanet.cs
+++ Abstractplanets/TerrestrialPlanet.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 * Date: July 28,2017
 * Description:It is a subclass of the Planet Abstract class. The class will also 
 implement both the IHasMoons and IHabitable interfaces.
-* Version: 0.1 - Created TerrestrialPlanet Class
+* Version: 0.2 - Added Private instance variables
 */
 namespace Abstractplanets
 {
-	class TerrestrialPlanet
+	class TerrestrialPlanet 
 	{
+		/*Private Instance Varibales*/
+		private bool _oxygen;
 	}
 }
