67c8a4f8b0c291550783fd0e52d531259cd63aaf
manmeet <manmeetrai2@gmail.com>
2017-07-28 18:49:30 -0400

    Added  constructor and a public overridden method in plant class

--- Abstractplanets/Planet.cs
+++ Abstractplanets/Planet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,16 @@ using System.Threading.Tasks;
 * Name: Manmeet Singh
 * Date: July 28,2017
 * Description: Abstract Planet Class
-* Version: 0.1 - Planet Class Created.
+* Version: 0.2 - Added constructor and overridden method.
 */
-namespace Abstractplanets
+
+namespace Assignment4_AbstractPlanets
 {
+	
 	public abstract class Planet
 	{
-		//Private instance variables
+
+		/* Private instance variables*/
 		private double _diameter;
 		private double _mass;
 		private int _moonCount;
@@ -22,79 +26,104 @@ namespace Abstractplanets
 		private double _orbitalPeriod;
 		private int _ringCount;
 		private double _rotationPeriod;
-	}
-	// PUBLIC PROPERTIES
 
-	public double Diameter
-	{
-		get
-		{
-			return this.diameter;
-		}
-	}
 
-	public double Mass
-	{
-		get
-		{
-			return this.mass;
-		}
-	}
+		/* public properties*/
 
-	public int MoonCount
-	{
-		get
+		public double Diameter
 		{
-			return this.moonCount;
-		}
-		set
-		{
-			this.moonCount = value;
-		}
-	}
-
-	public string Name
-	{
-		get
-		{
-			return this.name;
-		}
-	}
-
-	public double OrbitalPeriod
-	{
-		get
-		{
-			return this.orbitalPeriod;
-		}
-		set
-		{
-			this.orbitalPeriod = value;
-		}
-	}
-
-	public int RingCount
-	{
-		get
-		{
-			return this.ringCount;
-		}
-		set
-		{
-			this.ringCount = value;
-		}
-	}
-
-	public double RotationPeriod
-	{
-		get
-		{
-			return this.rotationPeriod;
+			get
+			{
+				return this._diameter;
+			}
 		}
 
-		set
+		public double Mass
 		{
-			this.rotationPeriod = value;
+			get
+			{
+				return this._mass;
+			}
+		}
+
+		public int MoonCount
+		{
+			get
+			{
+				return this._moonCount;
+			}
+			set
+			{
+				this._moonCount = value;
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return this._name;
+			}
+		}
+
+		public double OrbitalPeriod
+		{
+			get
+			{
+				return this._orbitalPeriod;
+			}
+			set
+			{
+				this._orbitalPeriod = value;
+			}
+		}
+
+		public int RingCount
+		{
+			get
+			{
+				return this._ringCount;
+			}
+			set
+			{
+				this._ringCount = value;
+			}
+		}
+
+		public double RotationPeriod
+		{
+			get
+			{
+				return this._rotationPeriod;
+			}
+
+			set
+			{
+				this._rotationPeriod = value;
+			}
+		}
+
+		
+
+		//Construcor
+		public Planet(string name, double diameter, double mass)
+		{
+			this._name = name;
+			this._diameter = diameter;
+			this._mass = mass;
+		}
+
+		//Public overridden metod
+
+		public override string ToString()
+		{
+			string outputString = "";
+
+			outputString = "Name:" + Name + "\n" + "Diameter:" + Diameter + "\n" + "Mass:" + Mass + "M" + "\n";
+
+			return outputString;
+
 		}
 	}
 }
+
