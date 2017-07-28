6c061c259e37eab4059c3fa935d2778ca8cf23b9
manmeet <manmeetrai2@gmail.com>
2017-07-28 18:29:46 -0400

    public abstract class Planet created, Private instance variables and public propertis added

---
+++ Abstractplanets/Planet.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Name: Manmeet Singh
+* Date: July 28,2017
+* Description: Abstract Planet Class
+* Version: 0.1 - Planet Class Created.
+*/
+namespace Abstractplanets
+{
+	public abstract class Planet
+	{
+		//Private instance variables
+		private double _diameter;
+		private double _mass;
+		private int _moonCount;
+		private string _name;
+		private double _orbitalPeriod;
+		private int _ringCount;
+		private double _rotationPeriod;
+	}
+	// PUBLIC PROPERTIES
+
+	public double Diameter
+	{
+		get
+		{
+			return this.diameter;
+		}
+	}
+
+	public double Mass
+	{
+		get
+		{
+			return this.mass;
+		}
+	}
+
+	public int MoonCount
+	{
+		get
+		{
+			return this.moonCount;
+		}
+		set
+		{
+			this.moonCount = value;
+		}
+	}
+
+	public string Name
+	{
+		get
+		{
+			return this.name;
+		}
+	}
+
+	public double OrbitalPeriod
+	{
+		get
+		{
+			return this.orbitalPeriod;
+		}
+		set
+		{
+			this.orbitalPeriod = value;
+		}
+	}
+
+	public int RingCount
+	{
+		get
+		{
+			return this.ringCount;
+		}
+		set
+		{
+			this.ringCount = value;
+		}
+	}
+
+	public double RotationPeriod
+	{
+		get
+		{
+			return this.rotationPeriod;
+		}
+
+		set
+		{
+			this.rotationPeriod = value;
+		}
+	}
+}
--- Abstractplanets/Program.cs
+++ Abstractplanets/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+* Name: Manmeet Singh
+* Date: July 28,2017
+* Description: This is the driver program class.
+* Version: 0.1 -Program class created
+*/
 namespace Abstractplanets
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+
 		}
 	}
 }
