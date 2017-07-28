0c9b6d0f81c49043ec9473fc35c80dbe21c45df4
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:16:51 -0400

    Class IHasMoons Created

--- Abstractplanets/IHasMoons.cs
+++ Abstractplanets/IHasMoons.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/* Name: Manmeet Singh
+* Date: July 28,2017
+* Description: The IHasMoons interface.
+* Version: 0.1 - IHasMoons Created
+*/
 
 namespace Abstractplanets
 {
-	class IHasMoons
-	{
-	}
+	
 }
