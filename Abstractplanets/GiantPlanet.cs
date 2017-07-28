3f38c2682a4fd78fff645d47c481ea1842e1246e
manmeet <manmeetrai2@gmail.com>
2017-07-28 18:54:27 -0400

    Giant Planet Class Created

---
+++ Abstractplanets/GiantPlanet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Name: Manmeet Singh
+* Date: July 28,2017
+* Description: This class  is a subclass of the Planet Abstract class. The class will also 
+implement both the IHasMoons and IHasRings interfaces
+* Version: 0.1 Giant Planet Class Created 
+*/
+namespace Abstractplanets
+{
+	class GiantPlanet
+	{
+	}
+}
