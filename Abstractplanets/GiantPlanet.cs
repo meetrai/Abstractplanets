878ae7a489f343cf45799d633913a75161dd22f5
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:10:59 -0400

    Added Constructor and Public methods to the The Giant Planet Class

--- Abstractplanets/GiantPlanet.cs
+++ Abstractplanets/GiantPlanet.cs
@@ -9,11 +9,44 @@ using System.Threading.Tasks;
 * Date: July 28,2017
 * Description: This class  is a subclass of the Planet Abstract class. The class will also 
 implement both the IHasMoons and IHasRings interfaces
-* Version: 0.1 Giant Planet Class Created 
+* Version: 0.2 Added Constructor and Public methods to the class
 */
 namespace Abstractplanets
 {
-	class GiantPlanet
+	class GiantPlanet : Planet
 	{
+		/*Private instance variables*/
+		private string _type;
+		/*Constructor*/
+		public GiantPlanet(string name, double diameter, double mass, string type)
+				: base(name, diameter, mass)
+		{
+			this._type = type;
+		}
+
+		//Public Methods
+		public bool HasMoons()
+		{
+			if (MoonCount > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public bool HasRings()
+		{
+			if (RingCount > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 }
