089d1bcd7b86ee860f60ffabc0e6288b0e016b2d
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:18:13 -0400

    Added IHasMoons Interface In class

--- Abstractplanets/IHasMoons.cs
+++ Abstractplanets/IHasMoons.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 /* Name: Manmeet Singh
 * Date: July 28,2017
 * Description: The IHasMoons interface.
-* Version: 0.1 - IHasMoons Created
+* Version: 0.2 - Added IHasMoons Interface
 */
 
 namespace Abstractplanets
 {
-	
+	/*Interface with HasMoons Method*/
+	interface IHasMoons
+	{
+
+		bool HasMoons();
+	}
 }
