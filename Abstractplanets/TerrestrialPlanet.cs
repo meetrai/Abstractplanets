a1f1dd4ba6266423a4ee7ebf5e2a94b4feee2f85
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:36:29 -0400

    Created terrestrialPlanet Class

---
+++ Abstractplanets/TerrestrialPlanet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Manmeet Singh
+* Date: July 28,2017
+* Description:It is a subclass of the Planet Abstract class. The class will also 
+implement both the IHasMoons and IHabitable interfaces.
+* Version: 0.1 - Created TerrestrialPlanet Class
+*/
+namespace Abstractplanets
+{
+	class TerrestrialPlanet
+	{
+	}
+}
