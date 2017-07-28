74140e500c18ae5cc8b1cf73738420fe55e70b82
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:21:35 -0400

    Created IHasRings Class

--- Abstractplanets/IHasRings.cs
+++ Abstractplanets/IHasRings.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Name: Manmeet Singh
+* Date: July 28,2017
+* Description: The IHasRings interface.
+* Version: 0.1 - Created IHasRings Class
+*/
 namespace Abstractplanets
 {
 	class IHasRings
