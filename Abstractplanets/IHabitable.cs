2553abab4cc9142124c3cfea6d7801d4992f60b4
manmeet <manmeetrai2@gmail.com>
2017-07-28 19:30:32 -0400

    Created IHabitable Interface

---
+++ Abstractplanets/IHabitable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Manmeet Singh
+* Date: July 28,2017
+* Description: The IHabitable interface.
+* Version: 0.1 - Created IHabitable Interface
+*/
+
+namespace Abstractplanets
+{
+	
+
+}
